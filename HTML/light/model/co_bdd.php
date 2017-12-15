bf41a513cff3e71bc29b7d021414141414052c36
Scorpiz <31989190+Scorpiz@users.noreply.github.com>
2017-12-15 15:44:26 +0100

    changement de bdd

--- HTML/light/model/co_bdd.php
+++ HTML/light/model/co_bdd.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $bdd = new PDO('mysql:host=localhost;dbname=m2l;charset=utf8', 'root', '');
+        $bdd = new PDO('mysql:host=172.16.0.2;dbname=tgorszczyk;charset=utf8', 'tgorszczyk', 'rngjesus77');
     }catch(Exception $e){
-        die("Erreur de connexion à la BDD : $e->getMessage()");
+        die("Erreur de connexion à la BDD");
     }
