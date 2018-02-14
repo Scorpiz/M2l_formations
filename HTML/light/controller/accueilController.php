e527e86eb29d6c01c5a6d5b367e8504df7177f39
ThomasGo <Thomasgo_1@hotmail.fr>
2018-02-14 10:31:54 +0100

    my commit

--- HTML/light/controller/accueilController.php
+++
@@ -1,6 +0,0 @@
-<?php require "../model/accueilModel.php"; ?>
-<?php
-
-
-?>
-<?php require "../view/accueilView.php"; ?>
--- HTML/light/model/co_bdd.php
+++
@@ -1,6 +0,0 @@
-<?php
-    try{
-        $bdd = new PDO('mysql:host=172.16.0.2;dbname=tgorszczyk;charset=utf8', 'tgorszczyk', 'rngjesus77');
-    }catch(Exception $e){
-        die("Erreur de connexion à la BDD");
-    }
